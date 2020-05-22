bb6695521bcb28036eca53198a82b66b6690e78a
amitjamwal85@gmail.com <amitjamwal85@gmail.com>
2020-05-22 11:49:41 +0530

    update

--- DjangoDRF/settings.py
+++ DjangoDRF/settings.py
@@ -183,9 +183,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 """
 User for login_required decorator 
 """
 LOGIN_URL = '/webapp/login/'
 LOGIN_REDIRECT_URL = '/webapp/login/'
+
+
